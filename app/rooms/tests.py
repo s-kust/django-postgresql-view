edcbb71f772ac054ea2c32217d9ef1ae2a074617
root <root@DESKTOP-JRMIVKT.localdomain>
2022-04-14 11:34:33 +0200

    Added tests for furniture m2m_relations changes

--- app/rooms/tests.py
+++ app/rooms/tests.py
@@ -3,7 +3,7 @@ import random
 import string
 import pytest
 
-from rooms.models import Door, Room, RoomsRelatedObjectsMaterializedView, RoomWithRelatedObjsRebuildInApp
+from rooms.models import Door, Room, RoomsRelatedObjectsMaterializedView, RoomWithRelatedObjsRebuildInApp, Chair, Table, Bed
 
 logger = logging.getLogger(__name__)
 
@@ -46,3 +46,82 @@ def test_room_change_reflected_in_its_related_models():
     room_view.refresh_from_db()
     room_2.refresh_from_db()
     assert room.name == room_view.name == room_2.name == new_room_name
+
+
+def _check_item_in_room_view_and_v2(item_id, furniture_class_name, room_view_obj, room_v2_obj):
+    if furniture_class_name == 'Bed':
+        furniture_in_room_view = room_view_obj.beds
+        furniture_in_room_v2 = room_v2_obj.beds
+    if furniture_class_name == 'Chair':
+        furniture_in_room_view = room_view_obj.chairs
+        furniture_in_room_v2 = room_v2_obj.chairs
+    if furniture_class_name == 'Table':
+        furniture_in_room_view = room_view_obj.tables
+        furniture_in_room_v2 = room_v2_obj.tables
+    item_in_room_view_found = False
+    item_in_room_v2_found = False
+    if len(furniture_in_room_view) == 0 or len(furniture_in_room_v2) == 0:
+        return item_in_room_view_found, item_in_room_v2_found
+    for elem in furniture_in_room_view:
+        if elem['id'] == item_id:
+            item_in_room_view_found = True
+    for elem in furniture_in_room_v2:
+        if elem['id'] == item_id:
+            item_in_room_v2_found = True
+    return item_in_room_view_found, item_in_room_v2_found
+
+
+def _get_furniture_item_of_class(furniture_class_name):
+    if furniture_class_name == 'Bed':
+        items = list(Bed.objects.all())
+    if furniture_class_name == 'Chair':
+        items = list(Chair.objects.all())
+    if furniture_class_name == 'Table':
+        items = list(Table.objects.all())
+    instance = random.choice(items)
+    return instance
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("furniture_class", ["Bed", "Chair", "Table"])
+def test_furniture_rooms_set_remove_reflected(furniture_class):
+    item = _get_furniture_item_of_class(furniture_class)
+    room = item.rooms.first()
+    room_view = RoomsRelatedObjectsMaterializedView.objects.get(pk=room.id)
+    room_v2 = RoomWithRelatedObjsRebuildInApp.objects.get(pk=room.id)
+    item_in_room_view_found, item_in_room_v2_found = _check_item_in_room_view_and_v2(
+        item.id, furniture_class, room_view, room_v2)
+    assert item_in_room_view_found
+    assert item_in_room_v2_found
+    item.rooms.remove(room)
+    room_view.refresh_from_db()
+    room_v2.refresh_from_db()
+    item_in_room_view_found, item_in_room_v2_found = _check_item_in_room_view_and_v2(
+        item.id, furniture_class, room_view, room_v2)
+    assert not item_in_room_view_found
+    assert not item_in_room_v2_found
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize("furniture_class", ["Bed", "Chair", "Table"])
+def test_furniture_rooms_set_add_reflected(furniture_class):
+    item = _get_furniture_item_of_class(furniture_class)
+    all_rooms = list(Room.objects.all())
+    found_room_not_in_item_rooms = False
+    while not found_room_not_in_item_rooms:
+        room = random.choice(all_rooms)
+        if not room in item.rooms.all():
+            found_room_not_in_item_rooms = True
+    room_view = RoomsRelatedObjectsMaterializedView.objects.get(pk=room.id)
+    room_v2 = RoomWithRelatedObjsRebuildInApp.objects.get(pk=room.id)
+    item_in_room_view_found, item_in_room_v2_found = _check_item_in_room_view_and_v2(
+        item.id, furniture_class, room_view, room_v2)
+    assert not item_in_room_view_found
+    assert not item_in_room_v2_found
+    item.rooms.add(room)
+    room_view.refresh_from_db()
+    room_v2.refresh_from_db()
+    item_in_room_view_found, item_in_room_v2_found = _check_item_in_room_view_and_v2(
+        item.id, furniture_class, room_view, room_v2)
+    assert item_in_room_view_found
+    assert item_in_room_v2_found
